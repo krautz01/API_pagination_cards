61e610ec086a4e0d012a020d2d159c827fe2ac82
krautz01 <bekturnurlanov01@gmail.com>
2022-02-13 15:25:15 +0600

    11

--- src/components/Pages.jsx
+++ src/components/Pages.jsx
@@ -5,76 +5,53 @@ import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { Button, CardActionArea, CardActions } from '@mui/material';
 import * as axios from 'axios';
+const BASE_URL = "http://jsonplaceholder.typicode.com/photos?"
 
 const Pages = (props) => {
-    /* let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
-    let pages = [
-        users: [ ],
-    pageSize: 5,
-    totalUsersCount: 0,
-    currentPage: 1,
-    ];
-
-    for (let i = 1; i <= pagesCount; i++) {
-        pages.push(i);
-    };
-
-    return <div className={styles.userBlock}>
-        <div> {pages.map(p => {
-            return <span className={props.currentPage === p && styles.selectedPage}
-                onClick={(e) => {
-                    props.onPageChanged(p);
-                }}>{p}</span>
-        })}
-         */
 
     const [Images, setImages] = useState([]);
-    const [currentPage, setCurrentPage] = useEffect(1);
-    const [totalUsersCount,] =
 
-
-        axios.get("http://jsonplaceholder.typicode.com/photos").then(response => {
-            setImages(response.data);
+    useEffect(() => {
+        axios.get(BASE_URL).then(response => {
+            setImages(response);
+            console.log(Images);
         });
+    })
 
     return (
         <div>
-            {
-                Images.map(Image => (
-                    <div>{
-                        <Card sx={{ maxWidth: 345 }} key={Image.id}>
-                            <CardActionArea>
-                                <CardMedia
-                                    component="img"
-                                    height="140"
-                                    src={Image.url}
-                                    alt="green iguana"
-                                />
-                                <CardContent>
-                                    <Typography gutterBottom variant="h5" component="div">
-                                        {Image.title}
-                                    </Typography>
-                                    <Typography variant="body2" color="text.secondary">
-                                        Description
-                                    </Typography>
-                                </CardContent>
-                            </CardActionArea>
-                            <CardActions>
-                                <Button size="small" color="primary">
-                                    Share
-                                </Button>
-                            </CardActions>
-                        </Card>
-                    }
-                    </div>
-                )
-
-
-
-                )
+            {/* {Images.map(Image => (
+                <div>{
+                    <Card sx={{ maxWidth: 345 }} key={Image.id}>
+                        <CardActionArea>
+                            <CardMedia
+                                component="img"
+                                height="140"
+                                src={Image.url}
+                                alt="green iguana"
+                            />
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="div">
+                                    {Image.title}
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary">
+                                    Description
+                                </Typography>
+                            </CardContent>
+                        </CardActionArea>
+                        <CardActions>
+                            <Button size="small" color="primary">
+                                Share
+                            </Button>
+                        </CardActions>
+                    </Card>
+                }
+                </div>
+            )
+            )
             }
 
-
+ */}
         </div>
     )
 };
